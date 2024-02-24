21ca94df6dd9ff049d9d604cfece9ff8560ac603
srushti84 <160316324+srushti84@users.noreply.github.com>
2024-02-24 19:41:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
-
+import streamlit as st
+x = st.slider('Select a value: ')
+y = st.number_input("Enter number", 0, 25, 6, 2)
+st.write(f'{x} + {y} = {x+y}')
+st.write(f'{x} - {y} = {x-y}')
+st.write(f'{x} * {y} = {x*y}')
+st.write(f'{x} / {y} = {x/y}')
